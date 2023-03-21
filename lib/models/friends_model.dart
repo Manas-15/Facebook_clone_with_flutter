383187b304b0ba5c427618ab47de8a1cd8f2d0a1
Manas Pasayat <andolasoft.user187@gmail.com>
2023-03-21 18:33:31 +0530

    added new friends data in model

--- lib/models/friends_model.dart
+++ lib/models/friends_model.dart
@@ -11,4 +11,9 @@ List<FriendsModel> friendsData = [
   FriendsModel(name: "Satya", avatar: 'images/user.png'),
   FriendsModel(name: "Shashanga", avatar: 'images/user.png'),
   FriendsModel(name: "Sapan", avatar: 'images/user.png'),
+  FriendsModel(name: "Aditya", avatar: 'images/user.png'),
+  FriendsModel(name: "Persenjit", avatar: 'images/user.png'),
+  FriendsModel(name: "Sandigdha", avatar: 'images/user.png'),
+  FriendsModel(name: "Ajay", avatar: 'images/user.png'),
+  FriendsModel(name: "Vicky", avatar: 'images/user.png'),
 ];
