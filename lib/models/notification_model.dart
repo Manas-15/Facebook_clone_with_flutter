f7a483f6947b5074ecd5d065e2e4291168a9f6f4
Manas Pasayat <andolasoft.user187@gmail.com>
2023-03-21 18:32:08 +0530

    completed notification screen UI

---
+++ lib/models/notification_model.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class NotificationModel {
+  final String name;
+  final String avatar;
+  final String time;
+  final String description;
+
+  NotificationModel({
+    required this.name,
+    required this.avatar,
+    required this.time,
+    required this.description,
+  });
+}
+
+List<NotificationModel> notificationData = [
+  NotificationModel(
+      name: "Sisa",
+      avatar: 'images/19722.jpg',
+      time: "9 Min ago",
+      description: "Like Manas"),
+  NotificationModel(
+      name: "Sisa",
+      avatar: 'images/19722.jpg',
+      time: "9 Min ago",
+      description: "Like Manas"),
+  NotificationModel(
+      name: "Sisa",
+      avatar: 'images/19722.jpg',
+      time: "9 Min ago",
+      description: "Like Manas"),
+  NotificationModel(
+      name: "Sisa",
+      avatar: 'images/19722.jpg',
+      time: "9 Min ago",
+      description: "Like Manas"),
+  NotificationModel(
+      name: "Sisa",
+      avatar: 'images/19722.jpg',
+      time: "9 Min ago",
+      description: "Like Manas"),
+  NotificationModel(
+      name: "Sisa",
+      avatar: 'images/19722.jpg',
+      time: "9 Min ago",
+      description: "Like Manas"),
+  NotificationModel(
+      name: "Sisa",
+      avatar: 'images/19722.jpg',
+      time: "9 Min ago",
+      description: "Like Manas"),
+  NotificationModel(
+      name: "Sisa",
+      avatar: 'images/19722.jpg',
+      time: "9 Min ago",
+      description: "Like Manas"),
+];
